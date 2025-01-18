e0ae6eddf4a4581f826a8874c506403abc4bced4
veerlakshay <lakshay.rv@gmail.com>
2025-01-18 13:46:34 -0500

    springboot backend added

---
+++ backend/CookWise/src/main/java/com/INFO_6134_01_25W_4/CookWise/CookWiseApplication.java
@@ -0,0 +1,13 @@
+package com.INFO_6134_01_25W_4.CookWise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookWiseApplication.class, args);
+	}
+
+}
---
+++ backend/CookWise/src/test/java/com/INFO_6134_01_25W_4/CookWise/CookWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.INFO_6134_01_25W_4.CookWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
